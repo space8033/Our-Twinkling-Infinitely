c8215d5cefea1aa44bdeda00f92c0d3758ae8a25
space8033 <space8237@naver.com>
2023-08-02 11:48:40 +0900

    chore: xml 파일 수정 및 테스트

--- src/main/java/com/webteam1/oti/Home.java
+++ src/main/java/com/webteam1/oti/Home.java
@@ -1,5 +1,12 @@
 package com.webteam1.oti;
 
-public class Home {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+public class Home {
+	@RequestMapping("/content")
+	public String content() {
+		return "list/list";
+	}
 }
