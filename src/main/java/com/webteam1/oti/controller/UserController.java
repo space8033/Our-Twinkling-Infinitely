f569bc9d733fbdd0b09790b716cfaa2138235dc8
space8033 <space8237@naver.com>
2023-08-04 09:06:49 +0900

    chore: test code 제거

--- src/main/java/com/webteam1/oti/controller/UserController.java
+++ src/main/java/com/webteam1/oti/controller/UserController.java
@@ -5,8 +5,6 @@ import javax.annotation.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.webteam1.oti.dto.Users;
-import com.webteam1.oti.dto.Users.UserType;
 import com.webteam1.oti.service.UserService;
 
 import lombok.extern.slf4j.Slf4j;
@@ -20,16 +18,6 @@ public class UserController {
 	
 	@RequestMapping("joinForm")
 	public String joinForm() {
-		Users user = new Users();
-		user.setUsers_id("실험용1");
-		user.setUsers_email("test1@mail.com");
-		user.setUsers_password("12345");
-		user.setUsers_name("팀1");
-		user.setUsers_phone("010-2222-3333");
-		user.setUsers_type(UserType.ENABLED);
-		user.setUsers_opoint(500);
-		
-		userService.join(user);
 		
 		return "join/joinForm";
 	}
