bb568cf2ced7121f1eaf6d28538dad7e8791a38a
yuyu062 <chorong6544@naver.com>
2023-08-12 15:12:18 +0900

    feat: ajax json데이터 넣기 완료

--- src/main/java/com/webteam1/oti/controller/CartController.java
+++ src/main/java/com/webteam1/oti/controller/CartController.java
@@ -22,6 +22,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.WebUtils;
 
 import com.webteam1.oti.dto.Product;
@@ -50,38 +51,6 @@ public class CartController {
 	@GetMapping("/cart")
 	public String menuCart() {
 		return "cart/basket";
-	}
-	
-	//장바구니에 담은 상품 리스트 불러오기
-	@ResponseBody
-	@GetMapping("/addCart")
-	public List<CartDto> cartView (HttpSession session, HttpServletRequest request, HttpServletResponse response, Cart cart, Model model) throws Exception {
-		Cookie cookie = WebUtils.getCookie(request, "cartCookie");
-	   List<CartDto> list = new ArrayList<>();
-	    log.info("실행함 나 진짜");
-	    //비회원시 쿠키value인 ckId사용
-	    if(cookie != null && session.getAttribute("loginIng")==null) {
-	    	log.info("웅 나 비회원~~");
-			//전에 받은 쿠키 id 값 삽입
-			String cartCookie = cookie.getValue();
-			log.info(cartCookie);
-			cart.setCart_ckId(cartCookie);
-			log.info("얍!!!!!!!!!!: " + cart);
-			list = cartService.getCartList(cart);
-			log.info("리스트 나와라 얍: " + list);
-		//회원시 users_id 이용
-	    }else if(cookie == null && session.getAttribute("loginIng") != null) {
-	    	log.info("웅 나 회원~~");
-			//로그인 세션 가져오기
-			LoginDto loginDto = (LoginDto) session.getAttribute("loginIng");
-			//장바구니에 로그인 한 user_id 삽입
-			cart.setUsers_users_id(loginDto.getUsers_id());
-			log.info("넣었쥬!");
-			list = cartService.getCartList(cart);
-			log.info("리스트 나와라 얍: " + list);
-	    }
-	    
-	    return list;
 	}
 
 	//상품 상세정보에서 장바구니 넣기 구현
