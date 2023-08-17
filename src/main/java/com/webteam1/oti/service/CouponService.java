871d8141d9241e395f80cf7835805c304509a6da
noismik <www_lol@naver.com>
2023-08-17 21:02:30 +0900

    feat: 주문결제 받아오기 및 order에 넘겨주기

--- src/main/java/com/webteam1/oti/service/CouponService.java
+++ src/main/java/com/webteam1/oti/service/CouponService.java
@@ -1,12 +1,15 @@
 package com.webteam1.oti.service;
 
+import java.util.List;
 
+import com.webteam1.oti.dto.Coupon;
 
 public interface CouponService {
 	public int generateWelcomeCoupon(String users_id);
 	public int generateBirthdayCoupon();
 	public int generateComeBackCoupon();
 	public void deleteExpiredCoupons();
+	public List<Coupon> getCouponByUsersId(String usersId);
 	
 /*	public void updateAddress(Address address);
 	public List<Address> getList(Map<String, Object> map);
