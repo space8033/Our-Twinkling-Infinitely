e93d93d06a0768be5602bda83f471eb31586ac0d
space8033 <space8237@naver.com>
2023-08-20 16:12:55 +0900

    feat: 예외 처리 기능 추가

---
+++ src/main/java/com/webteam1/oti/exception/MyOtiExceptionHandler.java
@@ -0,0 +1,34 @@
+package com.webteam1.oti.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+@Component
+@ControllerAdvice
+public class MyOtiExceptionHandler {
+	@ExceptionHandler(NullPointerException.class)
+	public String handleNullPointerException() {
+		return "exception/exception500";
+	}
+	
+	@ExceptionHandler(ClassCastException.class)
+	public String handleClassCastException() {
+		return "exception/exception500";
+	}
+	
+	@ExceptionHandler
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+	public String handleOtherException(Exception e) {
+		return "exception/exception500";
+	}
+	
+	@ExceptionHandler(NoHandlerFoundException.class)
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	public String handle404(NoHandlerFoundException e) {
+		return "exception/exception404";
+	}
+}
