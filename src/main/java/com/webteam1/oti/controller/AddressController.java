f3498b2b06daadf66e30175bc2595a1719ce3a0b
space8033 <space8237@naver.com>
2023-09-22 16:10:53 +0900

    feat: 모바일 주소지 검색기능 추가

--- src/main/java/com/webteam1/oti/controller/AddressController.java
+++ src/main/java/com/webteam1/oti/controller/AddressController.java
@@ -121,4 +121,9 @@ public class AddressController {
 		
 		return "mypage/address/myAddress";
 	}
+	
+	@GetMapping("/getDaumAddress")
+	public String getDaumAddress() {
+		return "mypage/address/daum";
+	}
 }
